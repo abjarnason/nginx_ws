9121a405dd84237fbfe42fba639e83981c72b6b9
Asgeir Bjarnason <asgbja@gmail.com>
2015-05-23 23:22:41 +0300

    add to github..

---
+++ build-and-run-container.sh
@@ -0,0 +1,4 @@
+docker build -t abjarnason/nginx_ws .
+docker stop nginx_ws
+docker rm nginx_ws
+docker run --name nginx_ws -d -p 80:80 --link ws_text_binary_server:ws_text_binary_server -v $PWD:/root abjarnason/nginx_ws
